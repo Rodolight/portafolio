18b73a84b13c806117ea3aa6215a1e03c53794bd
Rodolfo De Pena <rde_pena@outlook.com>
2018-04-21 14:36:44 -0400

    Servicio de fireBase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -4,17 +4,35 @@ import {Http} from "@angular/http";
 @Injectable()
 export class InformacionService {
    info:any = {};
+   equipo:any []=[];
+
    cargada:boolean=false;
+   cargada_sobre_nosotros:boolean=false;
 
   constructor(public http:Http) {
-    this.http.get("assets/data/info.pagina.json")
-    .subscribe( data =>{
-      console.log(data.json());
-      this.cargada=true;
-      this.info = data.json();
 
-    })
+    this.carga_sobre_nosotros();
+    this.carga_info();
 
    }
+
+  public carga_info(){
+     this.http.get("assets/data/info.pagina.json")
+     .subscribe( data =>{
+       this.cargada=true;
+       this.info = data.json();
+
+     });
+  }
+
+  public carga_sobre_nosotros(){
+
+  this.http.get("https://paginaweb-dbbc4.firebaseio.com/equipo.json")
+  .subscribe( data =>{
+    this.cargada_sobre_nosotros=true;
+    this.equipo = data.json();
+
+  });
+}
 
 }
